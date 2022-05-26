cc4c51839938fd7e716565ba79249fa9502f3e92
John Snow <skvirl@gmail.com>
2022-05-26 12:59:37 +0300

    theme trigger moved to js

--- 10-lection8/01-theme-changer/theme-changer.js
+++ 10-lection8/01-theme-changer/theme-changer.js
@@ -1,6 +1,10 @@
-const changer = document.getElementById('theme-changer');
+const changer1 = document.getElementById('theme-changer-1');
+const changer2 = document.getElementById('theme-changer-2');
 const html = document.documentElement;
 
-changer.addEventListener('change', function() {
+let dark_theme_state = () => {
     html.toggleAttribute('data-theme-dark');
-});
+   console.log("1111"); 
+}
+changer1.addEventListener('change', dark_theme_state );
+changer2.addEventListener('change', dark_theme_state );
